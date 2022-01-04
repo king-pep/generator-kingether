0ddac7673960cdaa5a3a66d7ed0d75a9881ffe7c
Abel Morris Ben <javaprojects756@gmail.com>
2022-01-04 13:24:46 +0200

    springboot generator

---
+++ README.md
@@ -0,0 +1,90 @@
+# generator-springboot
+A Yeoman generator for generating Microservices with SpringBoot
+
+## How to use?
+
+```
+> npm install -g yo
+> npm install -g generator-springboot
+> yo springboot
+```
+
+## Features
+
+* SpringBoot REST API with jar type packaging
+* CORS configuration
+* Swagger UI Integration
+* Spring Data JPA integration with option to select databases like MySQL, Postgresql, MariaDB etc
+* Flyway or Liquibase data migration support
+* SpringBoot Actuator configuration
+* TestContainers integration
+* JUnit 5 
+* Docker configuration for Application, ELK, Prometheus, Grafana
+* Localstack configuration
+* Jenkinsfile
+* GitHub Actions Configuration
+* CheckStyle, PMD, SonarQube, Google-java-format static analysis tools configuration
+
+
+### Generate SpringBoot Microservice
+
+![Microservice Generation](docs/server-generation-1.png)
+![Microservice Generation](docs/server-generation-2.png)
+
+### Generate REST API with CRUD operations
+You can generate REST API with CRUD operation using the following command:
+
+:high_brightness: You should run the following command from within the generated project folder. 
+
+`myservice> yo springboot:controller Customer --base-path /api/customers`
+
+This will generate:
+* JPA entity
+* Spring Data JPA Repository
+* Service
+* Spring MVC REST Controller with CRUD operations
+* Unit and Integration Tests for REST Controller
+* Flyway or Liquibase migration to create table
+
+![CRUD Generation](docs/crud-generation.png)
+
+
+## Why another generator when you have JHipster?
+JHipster is an amazing SpringBoot application generator with lots and lots of cool features.
+However, there are certain JHipster features that does not fit for my preferences such as:
+
+1. I like *jar* packaging
+2. I like to use spring-boot-starter-* than configuring individual libraries
+3. I like to have an option to generate application without spring-security
+4. I prefer Flyway to Liquibase
+5. I like to have only minimum and required configuration ie no AsyncConfiguration, LocaleConfiguration, CacheConfiguration, Logstash Logging etc.
+6. I like .properties over .yml
+
+## Local Development Setup
+
+```
+> git clone https://github.com/sivaprasadreddy/generator-springboot.git
+> cd generator-springboot
+> npm install 
+> npm link
+> yo springboot
+```
+
+## Changelog
+
+### Version 0.0.5
+* Added support for generating docker-compose yml files for application, ELK, Prometheus, Grafana
+
+### Version 0.0.6
+* Updated to use testcontainers-spring-boot https://github.com/testcontainers/testcontainers-spring-boot
+* Generate Zipkin docker-compose file when Distributed Tracing is selected
+* Fixed Flyway/Liquibase db migration script generation issue
+* Added tests for sanity check
+
+### Version 0.0.7
+* Removed support for generation of `config-server` and `service-registry`
+* Updated SpringBoot and other libraries version
+
+### Version 0.0.8
+* Configured Checkstyle, PMD, SonarQube, google-java-format plugins
+* Added Localstack autoconfiguration support
---
+++ generators/controller/index.js
@@ -0,0 +1,123 @@
+'use strict';
+const BaseGenerator = require('../base-generator');
+const constants = require('../constants');
+const _ = require('lodash');
+
+module.exports = class extends BaseGenerator {
+
+    constructor(args, opts) {
+        super(args, opts);
+        this.configOptions = this.options.configOptions || {};
+
+        this.argument("entityName", {
+            type: String,
+            required: true,
+            description: "Entity name"
+        });
+
+        this.option('base-path', {
+            type: String,
+            desc: "Base URL path for REST Controller"
+        })
+    }
+
+    get initializing() {
+        this.logSuccess('Generating JPA entity, repository, service and controller');
+        return {
+            validateEntityName() {
+                const context = this.context;
+                console.log(`EntityName: ${this.options.entityName}, basePath: ${this.options.basePath}`);
+                //this.env.error("The entity name is invalid");
+            }
+        }
+    }
+
+    /*get prompting() {
+        return prompts.prompting;
+    }*/
+
+    configuring() {
+        this.configOptions = Object.assign({}, this.configOptions, this.config.getAll());
+        this.configOptions.basePath = this.options['base-path'];
+        this.configOptions.entityName = this.options.entityName;
+        this.configOptions.entityVarName = _.camelCase(this.options.entityName);
+        this.configOptions.tableName = _.lowerCase(this.options.entityName)+'s';
+        this.configOptions.supportDatabaseSequences =
+            this.configOptions.databaseType === 'h2'
+            || this.configOptions.databaseType === 'postgresql';
+    }
+
+    writing() {
+        this._generateAppCode(this.configOptions);
+        this._generateDbMigrationConfig(this.configOptions)
+    }
+
+    _generateAppCode(configOptions) {
+        const mainJavaTemplates = [
+            {src: 'entities/Entity.java', dest: 'entities/'+configOptions.entityName+'.java'},
+            {src: 'repositories/Repository.java', dest: 'repositories/'+configOptions.entityName+'Repository.java'},
+            {src: 'services/Service.java', dest: 'services/'+configOptions.entityName+'Service.java'},
+            {src: 'web/controllers/Controller.java', dest: 'web/controllers/'+configOptions.entityName+'Controller.java'},
+        ];
+        this.generateMainJavaCode(configOptions, mainJavaTemplates);
+
+        const testJavaTemplates = [
+            {src: 'web/controllers/ControllerTest.java', dest: 'web/controllers/'+configOptions.entityName+'ControllerTest.java'},
+            {src: 'web/controllers/ControllerIT.java', dest: 'web/controllers/'+configOptions.entityName+'ControllerIT.java'},
+        ];
+        this.generateTestJavaCode(configOptions, testJavaTemplates);
+    }
+
+    _generateDbMigrationConfig(configOptions) {
+
+        if(configOptions.dbMigrationTool === 'flywaydb') {
+            this._generateFlywayMigration(configOptions)
+        }
+
+        if(configOptions.dbMigrationTool === 'liquibase') {
+            this._generateLiquibaseMigration(configOptions);
+        }
+    }
+
+    _generateFlywayMigration(configOptions) {
+        const counter = configOptions[constants.KEY_FLYWAY_MIGRATION_COUNTER] + 1;
+        let vendor = configOptions.databaseType;
+        if(vendor === "mariadb") {
+            vendor = "mysql";
+        }
+        const scriptTemplate = configOptions.supportDatabaseSequences ?
+            "V1__new_table_with_seq.sql" : "V1__new_table_no_seq.sql";
+
+        this.fs.copyTpl(
+            this.templatePath('app/src/main/resources/db/migration/flyway/V1__new_table_with_seq.sql'),
+            this.destinationPath('src/main/resources/db/migration/h2/V'+counter+'__create_'+configOptions.tableName+'_table.sql'),
+            configOptions
+        );
+        this.fs.copyTpl(
+            this.templatePath('app/src/main/resources/db/migration/flyway/'+scriptTemplate),
+            this.destinationPath('src/main/resources/db/migration/'+vendor+
+                '/V'+counter+'__create_'+configOptions.tableName+'_table.sql'),
+            configOptions
+        );
+        const flywayMigrantCounter = {
+            [constants.KEY_FLYWAY_MIGRATION_COUNTER]: counter
+        };
+        this.config.set(flywayMigrantCounter);
+    }
+
+    _generateLiquibaseMigration(configOptions) {
+        const counter = configOptions[constants.KEY_LIQUIBASE_MIGRATION_COUNTER] + 1;
+        const scriptTemplate = configOptions.supportDatabaseSequences ?
+            "01-new_table_with_seq.xml" : "01-new_table_no_seq.xml";
+        this.fs.copyTpl(
+            this.templatePath('app/src/main/resources/db/migration/liquibase/changelog/'+scriptTemplate),
+            this.destinationPath('src/main/resources/db/migration/changelog/0'+counter+'-create_'+configOptions.tableName+'_table.xml'),
+            configOptions
+        );
+        const liquibaseMigrantCounter = {
+            [constants.KEY_LIQUIBASE_MIGRATION_COUNTER]: counter
+        };
+        //const updatedConfig = Object.assign({}, this.config.getAll(), liquibaseMigrantCounter);
+        this.config.set(liquibaseMigrantCounter);
+    }
+};
